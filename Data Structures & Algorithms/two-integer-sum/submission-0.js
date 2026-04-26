b8e9cb64816083bd10fd5cec7238872357cc519d
Ashish Singh <ashishcodes4@gmail.com>
2026-04-26 18:10:19 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const map = new Map();
+
+        for(let i = 0; i < nums.length; ++i){
+            let diff = target - nums[i];
+            if(map.has(diff)){
+                let idx = map.get(diff)
+                return [i, idx];
+            } else {
+                map.set(nums[i], i)
+            }
+        }
+    }
+}
